0dc7a276244174e08f1961f8a7aba873b8618b64
Olivia Grace <ograce2@my.bcit.ca>
2025-09-27 11:41:21 -0700

    debugging for vercel

--- server.js
+++ server.js
@@ -9,29 +9,29 @@ const NAME = "name";
 const OPEN_SPAN = '<span style="color:blue">';
 const CLOSE_SPAN = '</span>';
 
-class Server{
-    static startLocalServer(){
-        http.createServer(function (req, res){
-            Server.handleRequest(req,res);
+class Server {
+    static startLocalServer() {
+        http.createServer(function (req, res) {
+            Server.handleRequest(req, res);
         }).listen(8888);
     }
 
-    static handleRequest(req, res){
+    static handleRequest(req, res) {
         let q = url.parse(req.url, true);
-        res.writeHead(200, {"Content-Type":"text/html"});
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.write(OPEN_SPAN + messages.GREETING.replace(PLACEHOLDER, q.query[NAME]) + dt.Utils.getDate() + CLOSE_SPAN);
         res.end();
     }
 
-    static startVercelServer(){
+    static startVercelServer() {
         Server.handleRequest(req, res);
     }
 }
 
+module.exports = Server.startVercelServer;
 
-if (require.main === module){
+
+if (require.main === module) {
     Server.startLocalServer();
-} else{
-    module.exports = Server.startVercelServer;
-}
+} 
 
